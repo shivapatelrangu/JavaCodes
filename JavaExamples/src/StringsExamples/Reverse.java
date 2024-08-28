465928aca2553ebabb4a4eabcd7b4484548f7762
Shiva <2262923@cognizant.com>
2024-08-28 11:28:33 +0530

    java

---
+++ JavaExamples/src/StringsExamples/Reverse.java
@@ -0,0 +1,11 @@
+package StringsExamples;
+
+public class Reverse {
+	public static void main(String[] args) {
+		String s = "shiva";
+		StringBuilder sb = new StringBuilder(s);
+		sb.reverse();
+		System.out.println(sb);
+	}
+
+}
